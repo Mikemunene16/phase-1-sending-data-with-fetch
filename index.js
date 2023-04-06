58e25caa5e2b0377014d46b7629a5876b89eda27
Mike <mike.munene@student.moringaschool.com>
2023-04-06 11:19:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+const userData = {
+    name: "Steve",
+    email: "steve@steve.com",
+};
+
+const configurationObject = {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
+    body: JSON.stringify(userData),
+};
+
+
+function submitData() {
+    return fetch('http://localhost:3000/users', configurationObject)
+        .then(response => response.json())
+        .then(data => document.body.innerHTML = data.id)
+        .catch(error => document.body.innerHTML = error.message);
+}
+
+submitData();
